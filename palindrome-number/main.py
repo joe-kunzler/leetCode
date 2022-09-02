bd3ac81c738921c12ca4b5802eb696705ee1edc6
joe-kunzler <joseph.kunzler@protonmail.com>
2022-09-02 13:02:06 -0400

    Better solution for reverse string

--- palindrome-number/main.py
+++ palindrome-number/main.py
@@ -4,12 +4,11 @@ class Solution:
             return False
         else:
             int_string = str(x)
-            int_list = []
-            for i in int_string:
-                int_list.append(i)
-            int_list.reverse()
-            reversed_string = ''.join(int_list)
-            if int_string == reversed_string:
-                return True
-            else:
-                return False
+            length = len(int_string)
+            for i in range(length):
+                if int_string[i] == int_string[length - 1]:
+                    length = length - 1
+                    continue
+                else:
+                    return False
+            return True
